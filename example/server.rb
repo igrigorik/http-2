585d0e817b54ed91cc22d409458896f55ac21d1d
George <george@boost-media.com>
2016-11-17 08:32:13 -0800

    Fix non-secure server to run against h2spec (#77)
    Fix non-secure server to run against h2spec: using sendmsg prevents Ruby from buffering response data/h2 frames.

--- example/server.rb
+++ example/server.rb
@@ -50,7 +50,7 @@ loop do
   conn = HTTP2::Server.new
   conn.on(:frame) do |bytes|
     # puts "Writing bytes: #{bytes.unpack("H*").first}"
-    sock.write bytes
+    sock.is_a?(TCPSocket) ? sock.sendmsg(bytes) : sock.write(bytes)
   end
   conn.on(:frame_sent) do |frame|
     puts "Sent frame: #{frame.inspect}"
