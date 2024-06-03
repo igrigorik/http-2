fcc254a2b99beaa8afebc39754577dff875c4222
Matt Muller <mamuller@amazon.com>
2024-06-03 11:43:08 -0400

    Fix backwards incompatibility

---
+++ lib/http/2.rb
@@ -0,0 +1,5 @@
+# This file can be removed with a major version bump.
+# New require path would be `require 'http2'`
+# Alternatively, module could instead become HTTP::2 to match http-2
+# gem naming recommendations.
+require_relative '../http2'
