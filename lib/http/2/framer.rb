9f36edfba843908419bc5dc209b257f659e026f9
HoneyryderChuck <cardoso_tiago@hotmail.com>
2025-03-12 23:27:11 +0000

    Use #filter_map instead of custom #each_with_object injecting to an array

--- lib/http/2/framer.rb
+++ lib/http/2/framer.rb
@@ -181,8 +181,8 @@ module HTTP2
 
       {
         type: type,
-        flags: FRAME_FLAGS[type].each_with_object([]) do |(name, pos), acc|
-          acc << name if flags.anybits?((1 << pos))
+        flags: FRAME_FLAGS[type].filter_map do |name, pos|
+          name if flags.anybits?((1 << pos))
         end,
         length: length,
         stream: stream & RBIT
